e44fadf8021bcb6cbdf871f42a286fc543e6d628
Sepehrman <Sepehrman@icloud.com>
2023-04-01 00:42:21 -0700

    Added Multicasting code

---
+++ multicasting/receiver.py
@@ -0,0 +1,14 @@
+import socket
+
+MULTICAST_GROUP = '224.0.0.0' # multicast group IP address
+PORT = 5000      # multicast group port
+
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+receiver_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+receiver_socket.bind(('', PORT))
+mreq = socket.inet_aton(MULTICAST_GROUP) + socket.inet_aton('0.0.0.0')
+receiver_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+while True:
+    data, address = receiver_socket.recvfrom(1024)
+    print(f"Received data: {data.decode('utf-8')}")
---
+++ multicasting/sender.py
@@ -0,0 +1,10 @@
+import socket
+
+MULTICAST_GROUP = '224.0.0.0' # multicast group IP address
+PORT = 5007      # multicast group port
+msg = b"Hello, World!"
+
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sender_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+sender_socket.sendto(msg, (MULTICAST_GROUP, PORT))
