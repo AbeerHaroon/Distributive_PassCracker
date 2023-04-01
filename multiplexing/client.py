b4534bc96ebac418fabf704d087357206285762a
Abeer Haroon <abr.haroon@gmail.com>
2023-04-01 16:50:39 -0700

    client_server_response draft

--- multiplexing/client.py
+++ multiplexing/client.py
@@ -1,7 +1,9 @@
 import socket
 import GuessGen
+import sys
 
 SERVER_IP = '192.168.1.75'
+message = sys.argv[1]
 # create a TCP/IP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -9,18 +11,20 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (SERVER_IP, 5000) #hard coded
 client_socket.connect(server_address)
 
-# send "Hello World" to the server
-message = "what_is_love"
+# send secret message to the server
+# message = "what_is_love"
 client_socket.sendall(message.encode())
 
 # receive the server's response
 data = client_socket.recv(1024)
 print("received: ", data.decode())
 
-to_crack = data
+to_crack = data.encode()
+
+g = GuessGen.GuessGen()
+ans = g.crackCycle(to_crack)
+
+client_socket.sendall(str(ans).encode())
 
 # close the socket
 client_socket.close()
-
-g = GuessGen.GuessGen()
-g.crackCycle(to_crack)
--- multiplexing/server.py
+++ multiplexing/server.py
@@ -4,7 +4,7 @@ import sys
 import GuessGen
 
 SERVER_PORT = 5000
-unlock_pw = "what_is_love"
+unlock_pw = "please"
 
 if len(sys.argv) < 3 :
     print("please print number of machines being used")
@@ -12,44 +12,45 @@ if len(sys.argv) < 3 :
     print("(considering program name first arg)")
     sys.exit()
 
-numConnections = sys.argv[1]
+numConnections = int(sys.argv[1])
 if numConnections > 10 :
     print("maximum 10 connections")
     sys.exit()
 
-users = [] #List of users to crack
-numUsers = 1
-while(numUsers < len(sys.argv)):
-    users.append(sys.argv[numUsers])
-    numUsers +=1
-    #List of users added
+# users = [] #List of users to crack
+# numUsers = 1
+# while(numUsers < len(sys.argv)):
+#     users.append(sys.argv[numUsers])
+#     numUsers +=1
+#     #List of users added
 
-hashed_passes = []
+# hashed_passes = []
 
-for u in users:
-    #Module that will search for username in command line
-    with open('/etc/shadow') as f:
-        line = f.read()
-        print(line)
-        uName_Index = line.find(u)
-        if uName_Index != -1: #if username found
-            passIndexFirst = len(u) + 1
-            chop = line[uName_Index+(passIndexFirst):] #starting from first index where username found + add up to index of when pass starts 
-            colonAfterPass = chop.find(":") #find index of first occurrence of colon
-            hashed = chop[0:colonAfterPass] #excluded colon index
-            print("extracted hash:",end="")
-            print(hashed)
-            hashed_passes.append(hashed)
+# for u in users:
+#     #Module that will search for username in command line
+#     with open('/etc/shadow') as f:
+#         line = f.read()
+#         print(line)
+#         uName_Index = line.find(u)
+#         if uName_Index != -1: #if username found
+#             passIndexFirst = len(u) + 1
+#             chop = line[uName_Index+(passIndexFirst):] #starting from first index where username found + add up to index of when pass starts 
+#             colonAfterPass = chop.find(":") #find index of first occurrence of colon
+#             hashed = chop[0:colonAfterPass] #excluded colon index
+#             print("extracted hash:",end="")
+#             print(hashed)
+#             hashed_passes.append(hashed)
 
-solved_passes = []
-print("following hashed passwords found")
-print(hashed_passes)
+# solved_passes = []
+# print("following hashed passwords found")
+# print(hashed_passes)
 
-if len(hashed_passes) == 0:
-    print("no hash pass found")
-    sys.exit()
+# if len(hashed_passes) == 0:
+#     print("no hash pass found")
+#     sys.exit()
  
-to_crack = hashed_passes[0] #first password found for now
+# to_crack = hashed_passes[0] #first password found for now
+to_crack = "$y$j9T$oHjB961YNYYZaS/B9V5sr1$eqVAPSL4NpprMtfP9ie6jayp.rdiGsdFQPa/KRVOxm7" #yescrypt for 2!
 
 # create a TCP/IP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -83,7 +84,7 @@ while True:
             if data.decode() == unlock_pw:
                 print("recognized temp pass from client")
                 # echo the data back to the client
-                sock.sendall(to_crack)
+                sock.sendall(to_crack.encode())
             else:
                 # close the socket
                 sock.close()
