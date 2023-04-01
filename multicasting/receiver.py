6b7de74903832c5373001fabbc2eb4bc78c9607f
Abeer Haroon <abr.haroon@gmail.com>
2023-04-01 12:23:44 -0700

    tweaking original multicast code

--- multicasting/receiver.py
+++ multicasting/receiver.py
@@ -1,11 +1,15 @@
 import socket
+import struct
+import sys
+
 
 MULTICAST_GROUP = '225.4.2.0' # multicast group IP address
 PORT = 5000      # multicast group port
 
 receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-receiver_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 receiver_socket.bind(('', PORT))
+
+receiver_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 mreq = socket.inet_aton(MULTICAST_GROUP) + socket.inet_aton('0.0.0.0')
 receiver_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
--- multicasting/sender.py
+++ multicasting/sender.py
@@ -1,10 +1,13 @@
 import socket
+import struct
+import sys
 
-MULTICAST_GROUP = '224.0.0.0' # multicast group IP address
+MULTICAST_GROUP = '225.4.2.0' # multicast group IP address
 PORT = 5000      # multicast group port
 msg = b"Hello, World!"
 
 sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sender_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+ttl = struct.pack('b',2)
+sender_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 sender_socket.sendto(msg, (MULTICAST_GROUP, PORT))
